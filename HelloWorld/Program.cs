7b8b1dcff51d0715d0d11183aa1b3861a6fcb2d4
Drake Ferry <drakeferry@usf.edu>
2021-01-20 17:51:36 -0500

    Added comments to code

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,8 +6,10 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            //Program opens a message saying "Hello World"
             Console.WriteLine("Hello World!");
             Console.WriteLine("Click any key to start the program closing procedure");
+            //User is able to close program by clicking any key on their keyboard
             Console.ReadKey(true);
         }
     }
